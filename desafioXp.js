383686bb1badc1326fbe494e85780ac78c3940cf
Guilherme Klein Klug <107370334+ProfessorGuilhermeKK@users.noreply.github.com>
2024-06-30 02:02:53 -0300

    Add files via upload

---
+++ desafioXp.js
@@ -0,0 +1,20 @@
+let heroi = "Linkin ";
+let heroiXp = 1100;
+if(heroiXp<1000){
+    console.log("O heroi "+ heroi+"é ferro")
+}
+else if(heroiXp>1000 && heroiXp<2000){
+    console.log("O heroi "+ heroi+ "é bronze")}
+else if(heroiXp>2000 && heroiXp<5000){
+        console.log("O heroi "+ heroi+"é prata")}
+else if(heroiXp>6000 && heroiXp<7000){
+            console.log("O heroi "+ heroi+"é ouro")}
+else if(heroiXp>7000 && heroiXp<8000){
+                console.log("O heroi "+ heroi+"é platina")}
+else if(heroiXp>8000 && heroiXp<9000){
+                 console.log("O heroi "+ heroi+"é ascendente")}
+else if(heroiXp>9000 && heroiXp<10000){
+                console.log("O heroi "+ heroi+"é imortal")}
+else{
+    console.log("O heroi "+ heroi+"é Radiante")
+}
